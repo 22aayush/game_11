52052106d328d9d2321ecfc6b37272ada8ab3efb
aayush <goyalbindu578@gmail.com>
2024-06-16 13:37:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const score = document.querySelector('.score');
+const startscreen = document.querySelector('.startscreen');
+const gamearea = document.querySelector('.gamearea');
+
+console.log(gamearea);
